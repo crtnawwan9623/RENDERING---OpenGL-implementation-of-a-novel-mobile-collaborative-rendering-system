09edeb115e1108b522b363d56f5474dabdce2386
Ruitao Chen <37515653+crtnawwan9623@users.noreply.github.com>
2021-09-09 12:16:46 -0400

    Add files via upload

---
+++ src_GPU_provider3/build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/rchen328/OneDrive - The University of Western Ontario/render/objview_master/src_worker3/libobj.cpp" "C:/Users/rchen328/OneDrive - The University of Western Ontario/render/objview_master/src_worker3/build/CMakeFiles/main.dir/libobj.cpp.obj"
+  "C:/Users/rchen328/OneDrive - The University of Western Ontario/render/objview_master/src_worker3/main.cpp" "C:/Users/rchen328/OneDrive - The University of Western Ontario/render/objview_master/src_worker3/build/CMakeFiles/main.dir/main.cpp.obj"
+  "C:/Users/rchen328/OneDrive - The University of Western Ontario/render/objview_master/src_worker3/shader.cpp" "C:/Users/rchen328/OneDrive - The University of Western Ontario/render/objview_master/src_worker3/build/CMakeFiles/main.dir/shader.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "DEBUG"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src_GPU_provider3/build/start.bat
@@ -0,0 +1,3 @@
+@echo off
+echo Starting GPU provider3...
+start "" "main.exe" "data\al.obj"
